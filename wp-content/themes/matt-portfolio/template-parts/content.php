7bb3e4b83a1734cfe99d2c376346ef6db474c4ce
Matt @ Home <matt@learnedmedia.com>
2017-03-20 22:12:48 -0400

    added ACF

--- wp-content/themes/matt-portfolio/template-parts/content.php
+++ wp-content/themes/matt-portfolio/template-parts/content.php
@@ -13,14 +13,14 @@
 	<header class="entry-header">
 		<?php
 		if ( is_single() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
+			the_title( '<h2 class="entry-title">', '</h2>' );
 		else :
 			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
 		endif;
 
 		if ( 'post' === get_post_type() ) : ?>
 		<div class="entry-meta">
-			<?php matt_portfolio_posted_on(); ?>
+			<!-- <?php matt_portfolio_posted_on(); ?> -->
 		</div><!-- .entry-meta -->
 		<?php
 		endif; ?>
